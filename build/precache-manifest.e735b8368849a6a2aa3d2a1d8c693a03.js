bd79d29de6254b832857859ce5536fe13dd09fb9
Joyeuse34 <turinsanjar13@gmail.com>
2025-03-07 10:43:43 +0500

    Added GitHub Pages deployment

---
+++ build/precache-manifest.e735b8368849a6a2aa3d2a1d8c693a03.js
@@ -0,0 +1,18 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "68598edf6f820f851fa0c958876b1f33",
+    "url": "/index.html"
+  },
+  {
+    "revision": "ed3ebefe85aa40220b17",
+    "url": "/static/js/2.0fe013da.chunk.js"
+  },
+  {
+    "revision": "af76807a8780183a2538",
+    "url": "/static/js/main.ae5a7a71.chunk.js"
+  },
+  {
+    "revision": "10e117344dea3db99e26",
+    "url": "/static/js/runtime-main.b5766938.js"
+  }
+]);
